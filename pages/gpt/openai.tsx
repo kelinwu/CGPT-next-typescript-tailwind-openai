bcabfd74683841de72881c2b95dd02f771ab6ae9
Kelin Wu <kelinwu@live.com>
2023-01-30 13:21:10 -0500

    chore: gpt enhanced..

--- pages/gpt/openai.tsx
+++ pages/gpt/openai.tsx
@@ -1,5 +1,4 @@
-// import { GetServerSideProps } from "next";
-import { SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 interface PropsI {
   data: {
@@ -10,44 +9,43 @@ interface PropsI {
   };
 }
 
-// export default async function GptPage({ _data }: PropsI) {
-export default function GptPage() {
+const GptPage = () => {
   const [prompt, setPrompt] = useState<PropsI["data"] | undefined>();
-
   const [message, setMessage] = useState<string>("openai");
-
   const [inputValue, setInputValue] = useState("");
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleInputChange = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    if (inputValue === "") {
-      setError(true);
-    } else {
-      setMessage(inputValue);
-      setError(false);
-      // Do something with the input value here
+  const fetchData = async (query: string) => {
+    setLoading(true);
+    try {
+      const res = await fetch(`/api/gpt/${query}`);
+      const jsonData = (await res.json()) as PropsI["data"][];
+      setPrompt(jsonData[0]);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(`/api/gpt/${message}`);
-      const jsonData = (await res.json()) as PropsI["data"][];
-      console.log(jsonData);
-      return jsonData;
-    };
-    fetchData()
-      .then((r) => {
-        setPrompt(r[0]);
-      })
-      .catch(console.error);
+    fetchData(message);
   }, [message]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!inputValue) {
+      setError(true);
+    } else {
+      setMessage(inputValue);
+      setInputValue("");
+      setError(false);
+    }
+  };
 
   return (
     <div className="p-10">
@@ -55,8 +53,7 @@ export default function GptPage() {
         Chat<span className="text-blue-500">GPT</span> <div className="text-sm">Next.ts</div>
       </h1>
       <div className="pb-20">
-
-      {prompt && prompt.text}
+        {loading ? "Loading..." : prompt?.text}
       </div>
 
       <div className="flex flex-col">
@@ -64,14 +61,14 @@ export default function GptPage() {
           className={`border rounded py-6 px-3 ${
             error ? "border-red-500" : ""
           }`}
-          placeholder="text this app by keyin 1 keyword, ie: chatgpt or openai or nextjs or tailwind ..."
+          placeholder="Text this app by entering a keyword, e.g. chatgpt or openai or nextjs or tailwind..."
           type="text"
           value={inputValue}
           onChange={handleInputChange}
         />
         {error && <p className="text-red-500 text-xs">Please enter a value</p>}
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px"
           onClick={handleSubmit}
         >
           Submit
@@ -81,14 +78,4 @@ export default function GptPage() {
   );
 }
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   // const res = await fetch("http://localhost:3002/api/gpt/react");
-//   // const data = await res.json();
-//   // console.log(data);
-//   // pass data to the page via props
-//   return {
-//     props: {
-//       _data: []
-//     },
-//   };
-// };
+export default GptPage
